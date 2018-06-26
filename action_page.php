f6d6e2fa00b0e893368145b71d8ba3261c64ad65
Yakov5776 <yakov5776@gmail.com>
2018-06-26 08:41:30 -0400

    Delete action_page.php

--- action_page.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-header('Content-Type: application/json');
-$request = file_get_contents('https://discordapp.com/api/webhooks/454293701842436116/LpXNaXvEOprpewlFXOs2jnhBWqdWA98icCS8nv-j5EDduhkz8A1ClUrqPY-JKN9_b8Wd');
-$req_dump = print_r( $request, true );
-$fp = file_put_contents( '[fname]', $req_dump );
-?
